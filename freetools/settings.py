a9c96f60b8b5269bbef4397bfeea6cd43d844504
SalahEddineEddamouh <116490798+SalahEddineEddamouh@users.noreply.github.com>
2023-09-04 16:27:44 +0100

    Update settings.py

--- freetools/settings.py
+++ freetools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$lljniykx&j1(pg3bx^za(-@_h!+fp(hr&ybpdwzng0r^r##9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
